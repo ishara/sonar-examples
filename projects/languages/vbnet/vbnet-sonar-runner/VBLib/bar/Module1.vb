a6170a5f0e292abb44372fba4472db22077e2156
David RACODON <racodond@users.noreply.github.com>
2013-10-25 18:36:13 +0200

    Add code that triggers an issue with the default quality profile

--- projects/languages/vbnet/vbnet-sonar-runner/VBLib/bar/Module1.vb
+++ projects/languages/vbnet/vbnet-sonar-runner/VBLib/bar/Module1.vb
@@ -13,6 +13,9 @@ Friend Module Module1
 
     Sub Main()
         Dim myClazz As New MyClazz
+        Dim foo(1) As String
+        foo(0) = "foo"
+        foo(1) = "bar"
         Console.WriteLine("Hello, world! " & myClazz.MyProperty)
         Console.ReadLine()
     End Sub
