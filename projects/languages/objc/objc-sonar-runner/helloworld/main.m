87dccdca371e4059e0e7bb2abde6f9017eddc31d
Massimo Paladin <massimo.paladin@gmail.com>
2015-01-28 12:03:36 +0100

    Objective-C: example of code raising issues

--- projects/languages/objc/objc-sonar-runner/helloworld/main.m
+++ projects/languages/objc/objc-sonar-runner/helloworld/main.m
@@ -8,6 +8,13 @@
 int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
+        int c = 0;
+        if (argc == 1) {
+            c = 1; 
+        } else if (argc == 1) {
+            c = 2;
+        } else {
+        }
         Other *object = [[Other alloc] init];
         [object sayHello];
     }
