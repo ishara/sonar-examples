8effea9feb0d00d611236dc45e1ca81e9288984c
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-07-24 18:41:13 +0200

    C# Example Project: Replace by a minimalistic project

--- projects/languages/csharp/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ projects/languages/csharp/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalcMultiplyTest")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalcMultiplyTest")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7938a995-55ea-4872-adc1-853e55358446")]
+[assembly: Guid("3aa56df7-8648-444d-8a8f-e8348f8fef0c")]
 
 // Version information for an assembly consists of the following four values:
 //
